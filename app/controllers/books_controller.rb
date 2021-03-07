bc717a9b52bad7643723decc74538f8ee68d7c42
toku-kou8 <koichi.university@outlook.jp>
2021-03-07 07:19:13 +0000

    update show => edit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,8 +15,12 @@ class BooksController < ApplicationController
   
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id), notice: 'Book was successfully created.'
+    if book.save
+      redirect_to book_path(book.id), notice: 'Book was successfully created.'
+    else
+      render 'index'
+    end
+    
   end
   
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get '/books' => 'books#index'
   post '/books' => 'books#create'
   get '/books/:id' => 'books#show', as: 'book'
-  get '/books/:id/edit' => 'books#edit'
+  get '/books/:id/edit' => 'books#edit', as: 'edit_book'
   patch '/books/:id' => 'books#update', as: 'update_book'
   
   delete '/books' => 'books#destroy', as: 'destroy_book'
